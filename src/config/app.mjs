e42d59cfab5a7e1062269832e43bbc76ea61e7a1
Bartek <bkotarski1@gmail.com>
2019-12-09 19:55:15 +0100

    Error Handler

--- src/config/app.mjs
+++ src/config/app.mjs
@@ -3,6 +3,7 @@ import {database} from '../config/database.mjs';
 import {productRoute} from '../routes/products.mjs'
 import {userRoute} from '../routes/users.mjs'
 import {billRoute} from '../routes/bills.mjs'
+import {catchError} from '../middlewares/errors.mjs';
 
 export const app = express();
 
@@ -14,3 +15,5 @@ app.use(express.json());
 app.use('/products',productRoute);
 app.use('/users',userRoute);
 app.use('/bills',billRoute);
+
+app.use(catchError);
--- src/middlewares/errors.mjs
+++ src/middlewares/errors.mjs
@@ -1,8 +1,16 @@
 
 export function catchAsync(fn){
+
     return(req,res,next)=>{
         fn(req,res,next)
         .catch(err => next(err));
 
     }
 }
+
+export function catchError(err,req,res,next){
+    res.status(err.status ||500);
+    res.json({
+        message: 'My message error'
+    });
+};
